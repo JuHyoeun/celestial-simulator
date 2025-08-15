5754ec6418124131efa0bacf53f90be6977f6cec
hyoeun <hyoeunju83@gmail.com>
2025-08-15 12:25:55 +0900

    코드 수정: 구조체 배열 사용

--- celestialSimulator/1.c
+++ celestialSimulator/1.c
@@ -14,17 +14,10 @@ struct planet {
 };
 
 // 행성들의 위치 출력함수
-void printPlanet(struct planet mercury, struct planet venus, 
-				struct planet earth , struct planet mars, struct planet jupiter, 
-				struct planet saturn, struct planet uranus, struct planet neptune) {
-	printf("%s : (%.3f, %.3f)\n", mercury.name, mercury.x, mercury.y);
-	printf("%s : (%.3f, %.3f)\n", venus.name, venus.x, venus.y);
-	printf("%s : (%.3f, %.3f)\n", earth.name, earth.x, earth.y);
-	printf("%s : (%.3f, %.3f)\n", mars.name, mars.x, mars.y);
-	printf("%s : (%.3f, %.3f)\n", jupiter.name, jupiter.x, jupiter.y);
-	printf("%s : (%.3f, %.3f)\n", saturn.name, saturn.x, saturn.y);
-	printf("%s : (%.3f, %.3f)\n", uranus.name, uranus.x, uranus.y);
-	printf("%s : (%.3f, %.3f)\n", neptune.name, neptune.x, neptune.y);
+void printPlanet(struct planet planets[], int count) {
+	for (int i = 0; i < count; i++) {
+		printf("%s : (%.3f, %.3f)\n", planets[i].name, planets[i].x, planets[i].y);
+	}
 	printf("\n");
 }
 
@@ -38,35 +31,33 @@ void planet_position_cal(struct planet* a, int day) {
 int main(void) {
 	// sun을 (0, 0)으로, 2차원 사분면에서 행성의 위치를 표현
 	// 단위는 1AU
-	// 초기 위치는 x축 위에서 시작
+	// 초기 위치는 x축 위
 	int days;
+	int planet_count = 8;
 
-	struct planet mercury = { "mercury", 0.4, 87.97, 0.4, 0 };
-	struct planet venus = { "venus", 0.7, 224.7, 0.7, 0 };
-	struct planet earth = { "earth", 1, 365.26,  1, 0 };
-	struct planet mars = { "mars", 1.5, 686.96, 1.5, 0 };
-	struct planet jupiter = { "jupiter", 5.2, 4333.29, 5.2, 0 };
-	struct planet saturn = { "saturn", 9.5, 10756.2, 9.5, 0 };
-	struct planet uranus = { "uranus", 19.2, 30707.49, 19.2, 0 };
-	struct planet neptune = { "neptune", 30.1, 60223.35, 30.1, 0 };
+	// 코드 반복 줄이기 위해 구조체 배열 사용
+	struct planet planets[8] = {
+		{ "mercury", 0.4, 87.97, 0.4, 0 },
+		{ "venus",   0.7, 224.7, 0.7, 0 },
+		{ "earth",   1.0, 365.26, 1.0, 0 },
+		{ "mars",    1.5, 686.96, 1.5, 0 },
+		{ "jupiter", 5.2, 4333.29, 5.2, 0 },
+		{ "saturn",  9.5, 10756.2, 9.5, 0 },
+		{ "uranus", 19.2, 30707.49, 19.2, 0 },
+		{ "neptune",30.1, 60223.35, 30.1, 0 }
+	};
 
-	printf("n일 동안의 행성의 움직임을 구현합니다.\n");
+	printf("n일 동안의 행성의 움직임을 구현합니다. 단위: AU\n");
 	printf("n을 입력: ");
 	scanf("%d", &days);
 
 	// 하루마다 행성의 위치를 출력
-	// 수성
-	for (int i = 0; i < days; i++) {
-		planet_position_cal(&mercury, i);
-		planet_position_cal(&venus, i);
-		planet_position_cal(&earth, i);
-		planet_position_cal(&mars, i);
-		planet_position_cal(&jupiter, i);
-		planet_position_cal(&saturn, i);
-		planet_position_cal(&uranus, i);
-		planet_position_cal(&neptune, i);
+	for (int i = 0; i <= days; i++) {
+		for (int j = 0; j < planet_count; j++) {
+			planet_position_cal(&planets[j], i);
+		}
 		printf("%d일차 행성의 위치\n", i);
-		printPlanet(mercury, venus, earth, mars, jupiter, saturn, uranus, neptune);
+		printPlanet(planets, planet_count);
 	}
 
 	return 0;
